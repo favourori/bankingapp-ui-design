d2bc0034bd5b654f1af60138847e6bb5f5063fea
Favour Ori <orifavour2000@gmail.com>
2020-01-23 00:28:32 +0200

    added bottom nav

--- lib/home.dart
+++ lib/home.dart
@@ -4,30 +4,49 @@ import 'package:flutter/material.dart';
 import 'dashboard.dart';
 import 'header.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Container(
-          padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
-          child: ListView(
-            children: <Widget>[
-              SizedBox(
-                height: 30.0,
-              ),
-              Header(),
-              SizedBox(
-                height: 30.0,
-              ),
-              Dashboard(),
-              SizedBox(
-                height: 30.0,
-              ),
-              RecentActivities(),
-            ],
-          ),
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 0.0),
+        child: ListView(
+          children: <Widget>[
+            SizedBox(
+              height: 30.0,
+            ),
+            Header(),
+            SizedBox(
+              height: 20.0,
+            ),
+            Dashboard(),
+            SizedBox(
+              height: 40.0,
+            ),
+            RecentActivities(),
+          ],
         ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            title: Text("Home"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.payment),
+            title: Text("Payments"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.redeem),
+            title: Text("Plan"),
+          ),
+        ],
       ),
     );
   }
--- lib/recent.dart
+++ lib/recent.dart
@@ -9,8 +9,62 @@ class RecentActivities extends StatelessWidget {
         Text(
           "Recent Activity",
           style: TextStyle(fontSize: 17.0),
-        )
+        ),
+        SizedBox(
+          height: 30.0,
+        ),
+        Transaction()
       ],
+    );
+  }
+}
+
+class Transaction extends StatelessWidget {
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 15.0, vertical: 5.0),
+        child: ListTile(
+          contentPadding: EdgeInsets.all(0),
+          leading: CircleAvatar(
+            backgroundColor: Color(0xFFEDF8ED),
+            child: Icon(
+              Icons.receipt,
+              color: Color(0xFF00B55B),
+            ),
+            radius: 30.0,
+          ),
+          title: Text("Samuel Achem"),
+          subtitle: Text("Softcom Airtime"),
+          trailing: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              SizedBox(
+                height: 10.0,
+              ),
+              Text(
+                "\$1000",
+                style: TextStyle(
+                  color: Color(0xFF00B55B),
+                  fontSize: 17.0,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+              SizedBox(
+                height: 4.0,
+              ),
+              Text(
+                "Jan 22, 2020",
+                style: TextStyle(fontSize: 12.0, color: Colors.grey),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
