f0776e848fecad5e87b888f6fc5191c2e231dfd1
Favour Ori <orifavour2000@gmail.com>
2020-01-23 10:25:43 +0200

    status bar color fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './home.dart';
 
-void main() => runApp(MyApp());
+void main() {
+ 
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
