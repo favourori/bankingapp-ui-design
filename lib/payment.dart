6962ba1b289ebdd4d248e2ab45e8fa6d21fed5a3
Favour Ori <orifavour2000@gmail.com>
2020-01-23 02:46:41 +0200

    fixing issues with nested list view

--- lib/payment.dart
+++ lib/payment.dart
@@ -1,11 +1,19 @@
+import 'package:bankingapp/sliding_dash.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class Payment extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Text("Hello"),
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 20.0),
+      child: ListView(
+        children: <Widget>[
+          Text("Hello"),
+          Flexible(
+            child: SlidingDash(),
+          )
+        ],
       ),
     );
   }
---
+++ lib/sliding_dash.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SlidingDash extends StatefulWidget {
+  @override
+  _SlidingDashState createState() => _SlidingDashState();
+}
+
+class _SlidingDashState extends State<SlidingDash> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: <Widget>[],
+    );
+  }
+}
