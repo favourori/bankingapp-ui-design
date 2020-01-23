254ea7894bcbad81c29f821e960d365d1e8d837e
Favour Ori <orifavour2000@gmail.com>
2020-01-23 10:35:14 +0200

    used a package to change status bar color

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import './home.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
 void main() {
- 
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  void changeBarColor() async {
+    await FlutterStatusbarcolor.setStatusBarColor(Colors.white);
+    if (useWhiteForeground(Colors.white)) {
+      FlutterStatusbarcolor.setStatusBarWhiteForeground(true);
+    } else {
+      FlutterStatusbarcolor.setStatusBarWhiteForeground(false);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    changeBarColor();
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomeScreen(),
